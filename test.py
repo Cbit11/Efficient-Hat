f794b1b65301236486c366c48312e44bb21453f3
CJ <rathodchaitanya11@gmail.com>
2025-10-05 12:07:48 -0400

    test code

--- test.py
+++ test.py
@@ -29,15 +29,19 @@ hat= HAT(in_chans=config['network_g']['in_chans'], embed_dim=config['network_g']
          num_heads=config['network_g']['hat_num_heads'], window_size= config['network_g']['window_size'],compress_ratio=config['network_g']['compress_ratio'],
          squeeze_factor=config['network_g']['squeeze_factor'],conv_scale=config['network_g']['conv_scale'],overlap_ratio=config['network_g']['overlap_ratio'],
          mlp_ratio=config['network_g']['mlp_ratio'],upscale=config['network_g']['upscale'],resi_connection='1conv',upsampler='pixelshuffle')
-# chkpt= config['checkpoints']
+chkpt= config['checkpoints']
 test_data = Validation_dataset(test_data_pth, 4, 256)
 test_loader= DataLoader(test_data, batch_size=1)
 hat.to(device)
-for batch, images in enumerate(test_loader):
-    # hr_img= images['GT'].squeeze(0).numpy()*255.
-    # lr_img= images['LR'].squeeze(0).numpy()*255.
-    hr_tensor= images['GT']
-    hr_img= tensor2img(hr_tensor, rgb2bgr=False)
-    cv2.imwrite(f"/home/cj/new_network_modified/results/gt_img_{batch}.png", hr_img.astype(np.uint8))
-    # cv2.imwrite(f"/home/cj/new_network_modified/results/lq_img_{batch}.png", lr_img.astype(np.uint8))
+hat.eval()
+with torch.no_grad():
+    for batch, images in enumerate(test_loader):
+        hr_tensor= images['GT'].to(device)
+        lr_tensor= images['LR'].to(device)
+        sr_tensor= hat(lr_tensor)
+        sr_image= tensor2img(sr_tensor, rgb2bgr=False)
+        hr_image= tensor2img(hr_tensor, rgb2bgr=False)
+
+        cv2.imwrite(f"/home/cj/new_network_modified/results/gt_img_{batch}.png", hr_image)
+        cv2.imwrite(f"/home/cj/new_network_modified/results/sr_img_{batch}.png", sr_image)
     
