df6468c5931bb7385837e9d52b0c04fc125cdf67
CJ <rathodchaitanya11@gmail.com>
2025-10-23 03:26:10 -0400

    multi gpu training

--- main.py
+++ main.py
@@ -1,59 +1,120 @@
 import os
 import torch
 import torch.nn as nn
-import torch.functional as f
 import numpy as np
-from basicsr.losses.basic_loss import *
+import torch.multiprocessing as mp
 from torch.utils.data import DataLoader, Dataset
-from data.Custom_image_dataset import Train_dataset, Validation_dataset
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
 from tqdm import tqdm
-import yaml
+from basicsr.losses.basic_loss import *
+from basicsr.metrics.psnr_ssim import calculate_psnr_pt, calculate_ssim_pt
+from data.Custom_image_dataset import Train_dataset, Validation_dataset
 from arch.Efficient_HAT import HAT
 from data.data_util import parse_from_yaml
 from utils import get_loss, get_optimizer, get_scheduler, resume_training
-from basicsr.metrics.psnr_ssim import calculate_psnr_pt, calculate_ssim_pt
 from torch.utils.tensorboard import SummaryWriter
-from PIL import Image
 from train import train_step, validation_step
 
 
-def main():
-  
-  file_pth = "/home/cj/new_network_modified/options/Efficient_hat_X2.yaml"
-
-  config = parse_from_yaml(file_pth)
-  train_data_pth = config['datasets']['train']['dataroot_gt']
-  val_data_pth = config['datasets']['val']['dataroot_gt']
-  device= config['device'] if torch.cuda.is_available() else 'cpu'
-  gt_size= config["datasets"]['train']["gt_size"]
-  hat= HAT(in_chans=config['network_g']['in_chans'], embed_dim=config['network_g']['embed_dims'], depths= config['network_g']['hat_depth'],
-         num_heads=config['network_g']['hat_num_heads'], window_size= config['network_g']['window_size'],compress_ratio=config['network_g']['compress_ratio'],
-         squeeze_factor=config['network_g']['squeeze_factor'],conv_scale=config['network_g']['conv_scale'],overlap_ratio=config['network_g']['overlap_ratio'],
-         mlp_ratio=config['network_g']['mlp_ratio'],upscale=config['network_g']['upscale'],resi_connection='1conv',upsampler='pixelshuffle')
-
-  loss= get_loss(config)
-  optimizer= get_optimizer(config, hat.parameters())
-  lr_scheduler= get_scheduler(config,  hat.parameters())
-  writer = SummaryWriter()
-  epochs = config['train']['epoch']
-  train_data= Train_dataset(train_data_pth, scale = 4, gt_size=gt_size)
-  if val_data_pth != None:
-    val_data= Validation_dataset(val_data_pth, scale= 4, gt_size=gt_size)
-    val_loader= DataLoader(val_data, batch_size= config['val_dataloader']['batch_size'], shuffle = config['val_dataloader']['shuffle'])
-  train_loader= DataLoader(train_data, batch_size= config['train_dataloader']['batch_size'], shuffle = config['train_dataloader']['shuffle'])
+# ---------- DDP setup ----------
+def ddp_setup(rank: int, world_size: int):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    torch.cuda.set_device(rank)
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
 
 
-  if config['resume_training']:
-    hat= resume_training(config, hat)
-    print("Resuming Training")
+# ---------- Main training ----------
+def main(rank, world_size):
+    ddp_setup(rank, world_size)
 
-  for epoch in tqdm(range(epochs)):
-      train_step(hat, loss, optimizer, lr_scheduler, train_loader, device, epoch)
-      if epoch%10==0:
-          if val_data_pth!= None:
-           validation_step(hat, loss, val_loader, device)
-          torch.save(hat.state_dict(),f"/home/cj/new_network_modified/checkpoints/D2FK_{epoch}")
+    # Load config
+    file_pth = "/home/cj/new_network_modified/options/Efficient_hat_X2.yaml"
+    config = parse_from_yaml(file_pth)
 
-if __name__== '__main__':
-  main() 
-           
+    train_data_pth = config['datasets']['train']['dataroot_gt']
+    val_data_pth = config['datasets']['val']['dataroot_gt']
+    gt_size = config["datasets"]['train']["gt_size"]  # adjust if in YAML under 'train'
+    epochs = config['train']['epoch']
+
+    device = torch.device(f"cuda:{rank}")
+
+    # Model
+    hat = HAT(
+        in_chans=config['network_g']['in_chans'],
+        embed_dim=config['network_g']['embed_dims'],
+        depths=config['network_g']['hat_depth'],
+        num_heads=config['network_g']['hat_num_heads'],
+        window_size=config['network_g']['window_size'],
+        compress_ratio=config['network_g']['compress_ratio'],
+        squeeze_factor=config['network_g']['squeeze_factor'],
+        conv_scale=config['network_g']['conv_scale'],
+        overlap_ratio=config['network_g']['overlap_ratio'],
+        mlp_ratio=config['network_g']['mlp_ratio'],
+        upscale=config['network_g']['upscale'],
+        resi_connection='1conv',
+        upsampler='pixelshuffle'
+    ).to(device)
+
+    if config['resume_training']:
+        hat = resume_training(config, hat)
+        print(f"[Rank {rank}] Resuming training")
+
+    hat = DDP(hat, device_ids=[rank])
+
+    # Loss, optimizer, scheduler
+    loss_fn = get_loss(config)
+    optimizer = get_optimizer(config, hat.parameters())
+    lr_scheduler = get_scheduler(config, hat.parameters())
+
+    # Datasets and loaders
+    train_dataset = Train_dataset(train_data_pth, scale=4, gt_size=gt_size)
+    train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=config['train_dataloader']['batch_size'],
+        sampler=train_sampler,
+        num_workers=4,
+        pin_memory=True
+    )
+
+    val_loader = None
+    if val_data_pth is not None:
+        val_dataset = Validation_dataset(val_data_pth, scale=4, gt_size=gt_size)
+        val_sampler = DistributedSampler(val_dataset, num_replicas=world_size, rank=rank, shuffle=False)
+        val_loader = DataLoader(
+            val_dataset,
+            batch_size=config['val_dataloader']['batch_size'],
+            sampler=val_sampler,
+            num_workers=4,
+            pin_memory=True
+        )
+
+    # TensorBoard (rank 0 only)
+    writer = SummaryWriter() if rank == 0 else None
+
+    # Training loop
+    for epoch in range(epochs):
+        train_sampler.set_epoch(epoch)
+        train_step(hat, loss_fn, optimizer, lr_scheduler, train_loader, device, epoch)
+
+        if rank == 0 and epoch % 10 == 0:
+            if val_loader is not None:
+                validation_step(hat, loss_fn, val_loader, device)
+            torch.save(
+                hat.module.state_dict(),
+                f"/home/cj/new_network_modified/checkpoints/DDP_D2FK_{epoch}.pth"
+            )
+
+    if writer:
+        writer.close()
+
+    destroy_process_group()
+
+
+# ---------- Launch ----------
+if __name__ == "__main__":
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size,), nprocs=world_size, join=True)
--- train.py
+++ train.py
@@ -1,61 +1,87 @@
-import os
 import torch
-import torch.nn as nn 
-import torch.functional as f
-import numpy as np 
-from basicsr.losses.basic_loss import *
-from torch.utils.data import DataLoader, Dataset
-from data.Custom_image_dataset import Train_dataset, Validation_dataset
-from tqdm import tqdm
-import yaml
-from arch.Efficient_HAT import HAT
-from data.data_util import parse_from_yaml
-from utils import get_loss, get_optimizer, get_scheduler
+from torch.distributed import all_reduce, ReduceOp
 from basicsr.metrics.psnr_ssim import calculate_psnr_pt, calculate_ssim_pt
 from torch.utils.tensorboard import SummaryWriter
 
-writer= SummaryWriter()
+# create writer only on rank 0
+writer = None
 
-def train_step(model, loss_fn, optimizer,scheduler ,dataloader, device, epoch):
-    print(f'The model is training on {device}')
-    train_loss = 0.
-    model.to(device)
+def train_step(model, loss_fn, optimizer, scheduler, dataloader, device, epoch, rank=0, world_size=1):
+    if rank == 0:
+        print(f"[Rank {rank}] Training on {device}")
+
+    model.train()
+    train_loss = 0.0
+
     for batch, data in enumerate(dataloader):
         optimizer.zero_grad()
-        gt= data['GT'].to(device)
-        lr= data['LR'].to(device)
-        pred= model(lr)
-        loss= loss_fn(pred,gt)
-        train_loss+= loss
+        gt = data['GT'].to(device, non_blocking=True)
+        lr = data['LR'].to(device, non_blocking=True)
+
+        pred = model(lr)
+        loss = loss_fn(pred, gt)
+
         loss.backward()
         optimizer.step()
+
+        # Accumulate loss (as tensor)
+        train_loss += loss.detach()
+
+    # Average loss across GPUs
+    train_loss = train_loss / len(dataloader)
+    train_loss_tensor = torch.tensor(train_loss, device=device)
+    all_reduce(train_loss_tensor, op=ReduceOp.AVG)
+    avg_loss = train_loss_tensor.item()
+
+    # Step scheduler once per epoch
     scheduler.step()
-    train_loss/= len(dataloader)
-    writer.add_scalar("Loss", train_loss, epoch)
-    print(f" | Train loss:{train_loss:.2f}")
-    
-def validation_step(model, loss_fn, dataloader, device):
-  val_loss= 0
-  total_psnr= 0
-  total_ssim= 0
-  model.to(device)
-  model.eval()
-  with torch.no_grad():
-    for batch, data in enumerate(dataloader):
-      gt= data['GT'].to(device)
-      lr= data['LR'].to(device)
-      pred= model(lr)
-      loss= loss_fn(pred,gt)
-      val_loss+= loss
-      psnr= calculate_psnr_pt(pred, gt,crop_border= 0,test_y_channel = True)
-      ssim= calculate_ssim_pt(pred, gt, crop_border= 0,test_y_channel= True)
-      total_psnr+= psnr
-      total_ssim+= ssim
-      
-    total_psnr/= len(dataloader)
-    total_ssim/= len(dataloader)
-    val_loss/=  len(dataloader)
-    print(f"Validation Loss: {val_loss} | PSNR: {total_psnr} | SSIM: {total_ssim}")
-    writer.add_scalar("Validation PSNR", total_psnr)
-    writer.add_scalar("Validation_SSIM", total_ssim)
-    
+
+    # Only rank 0 logs
+    if rank == 0 and writer is not None:
+        writer.add_scalar("Loss/train", avg_loss, epoch)
+        print(f"[Epoch {epoch}] Train Loss: {avg_loss:.4f}")
+
+    return avg_loss
+
+
+def validation_step(model, loss_fn, dataloader, device, epoch=0, rank=0, world_size=1):
+    model.eval()
+    val_loss = 0.0
+    total_psnr = 0.0
+    total_ssim = 0.0
+
+    with torch.no_grad():
+        for batch, data in enumerate(dataloader):
+            gt = data['GT'].to(device, non_blocking=True)
+            lr = data['LR'].to(device, non_blocking=True)
+            pred = model(lr)
+
+            loss = loss_fn(pred, gt)
+            psnr = calculate_psnr_pt(pred, gt, crop_border=0, test_y_channel=True)
+            ssim = calculate_ssim_pt(pred, gt, crop_border=0, test_y_channel=True)
+
+            val_loss += loss.detach()
+            total_psnr += psnr
+            total_ssim += ssim
+
+    # Compute local averages
+    val_loss /= len(dataloader)
+    total_psnr /= len(dataloader)
+    total_ssim /= len(dataloader)
+
+    # Reduce across all GPUs (get global average)
+    val_loss_t = torch.tensor(val_loss, device=device)
+    psnr_t = torch.tensor(total_psnr, device=device)
+    ssim_t = torch.tensor(total_ssim, device=device)
+    all_reduce(val_loss_t, op=ReduceOp.AVG)
+    all_reduce(psnr_t, op=ReduceOp.AVG)
+    all_reduce(ssim_t, op=ReduceOp.AVG)
+
+    if rank == 0:
+        print(f"[Validation] Loss: {val_loss_t.item():.4f}, PSNR: {psnr_t.item():.2f}, SSIM: {ssim_t.item():.4f}")
+        if writer is not None:
+            writer.add_scalar("Validation/Loss", val_loss_t.item(), epoch)
+            writer.add_scalar("Validation/PSNR", psnr_t.item(), epoch)
+            writer.add_scalar("Validation/SSIM", ssim_t.item(), epoch)
+
+    return val_loss_t.item(), psnr_t.item(), ssim_t.item()
